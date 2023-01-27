6389c84ba20087d262823d484b1ce6b1f1745e50
Mahesh <Mahesh9785>
2023-01-27 14:17:25 +0530

    Practice on Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-binding-example';
+  title = 'Binding Example';
+  isDisabled=false;
+  output='This is Property Binding';
+  userInput='';
+  returnValue='';
+  out='';
+
+  //show() function sets the value of isDisabled to false so the input field can be enabled and also throws an alert message.
+  show(){
+    this.isDisabled=false;
+    alert('Hello');
+  }
+
+  //saveIt() function takes the user enteredValue and returns it using returnValue.
+  saveIt(enteredValue:string){
+    this.returnValue=enteredValue;
+    return this.returnValue;
+  }
+
+  //save() function sets the value of isDisabled to true so the input field can be disabled and returns the value of userInput as out
+  save(){
+    this.isDisabled=true;
+  }
+
 }
